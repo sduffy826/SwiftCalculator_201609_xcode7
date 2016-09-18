2eac991dcf0569eef14bfa12ead6e052f1fc7833
Sean Duffy <remi_96@hotmail.com>
2016-09-18 10:48:02 -0400

    Initial Commit

---
+++ SwiftCalculator_201609_xcode7/CalculatorBrain.swift
@@ -0,0 +1,125 @@
+//
+//  CalculatorBrain.swift
+//  SwiftCalculator_201609_xcode7
+//
+//  Created by Sean Regular on 9/16/16.
+//  Copyright © 2016 CS193p. All rights reserved.
+//
+
+import Foundation
+
+func add(op1: Double, op2: Double) -> Double {
+    return op1 + op2
+}
+
+// Understand the closure stuff in the dictionary, it's cool!!  Closures
+// are like inline functions (but have state)
+class CalculatorBrain {
+    private var accumulator = 0.0
+    private var pending: PendingBinaryOperationInfo?
+    
+    private var operations: Dictionary<String, Operation> = [
+        // You need to provide the associated value to the enum case
+        "π" : Operation.Constant(M_PI),
+        "e" : Operation.Constant(M_E),
+        
+        // Associate function for unary operations
+        "√" : Operation.UnaryOperation(sqrt),
+        
+        // Show using closure for next two :)
+        "≪" : Operation.UnaryOperation({ $0 * 2 }), // shift left is mult by 2
+        "±" : Operation.UnaryOperation({ -$0 }),
+        
+        "sin" : Operation.UnaryOperation(sin),
+        "cos" : Operation.UnaryOperation(cos),
+        "tan" : Operation.UnaryOperation(tan),
+        "log" : Operation.UnaryOperation(log),
+        
+        // This is without closure, just call method to do the add
+        "+" : Operation.BinaryOperation(add),
+        
+        // This is showing with closure, first is long form
+        "−" : Operation.BinaryOperation({ (op1: Double, op2: Double) -> Double in return op1 - op2 }),
+        
+        // More... it knows the arguments based on associated value
+        // so we can clean up more
+        "×" : Operation.BinaryOperation({ (op1, op2) in return op1 * op2 }),
+        
+        // Even more, closure use $0..$n as default args and since
+        // it knows return type we can simplify with just :)
+        "÷" : Operation.BinaryOperation({ $0 / $1 }),
+        
+        "=" : Operation.Equals
+    ]
+    
+    // Enum for the different types of operations on the
+    // calculator, some (most) cases have an associated value
+    private enum Operation {
+        case Constant(Double)
+        case UnaryOperation((Double) -> Double)
+        case BinaryOperation((Double, Double) -> Double)
+        case Equals
+    }
+    
+    // This is called when user hits a binary operation, it
+    // saves the accumulator and the function that was hit
+    private struct PendingBinaryOperationInfo {
+        var binaryFunction: (Double, Double) -> Double
+        var firstOperand: Double
+    }
+    
+    // Used to set the current accumulator value (needed in controller)
+    func setOperand(operand: Double) {
+        accumulator = operand
+    }
+    
+    // This function executes the operation the user requested
+    // it uses the operations dictionary to determine the type of
+    // operation (an enum), then uses the associated value for the
+    // enum case to carry out the operation... only one a little 
+    // tricky is for binary operations we need to save the accumulator
+    // and function they requested (in pending...)
+    func performOperation(symbol: String) {
+        if let operation = operations[symbol] {
+            switch operation {
+            // Use (let xxx) to get the associated value
+            case .Constant(let value) :
+                accumulator = value
+            
+            // Set the accumulator to the value from the
+            // function call with the current accumulator
+            case .UnaryOperation(let function) :
+                accumulator = function(accumulator)
+                
+            // Set pending struct to be function and the current
+            // accumulator value
+            case .BinaryOperation(let function) :
+                executePendingBinaryOperation()
+                pending = PendingBinaryOperationInfo(binaryFunction: function, firstOperand: accumulator)
+                
+            // If a pending operation exists then we'll call
+            // it passing in the old accumulator value and the
+            // new one
+            case .Equals :
+                executePendingBinaryOperation()
+            }
+        }
+    }
+    
+    // This executes the pending operation, made function since
+    // it's called in several places :)
+    private func executePendingBinaryOperation() {
+        if pending != nil {
+            accumulator = pending!.binaryFunction(pending!.firstOperand, accumulator)
+            pending = nil
+        }
+    }
+    
+    // Used in the controller to get the result of operation (or
+    // value in accumulator... it's read only externally)
+    var result: Double {
+        get {
+            return accumulator
+        }
+    }
+}
--- SwiftCalculator_201609_xcode7/ViewController.swift
+++ SwiftCalculator_201609_xcode7/ViewController.swift
@@ -9,17 +9,61 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    private var userInMiddleOfTypeANumber = false
+    
+    @IBOutlet weak private var display: UILabel!
+    
+    // This computed value makes it easy to use the
+    // 'display'
+    private var displayValue: Double {
+        set {
+            display.text = String(newValue)
+        }
+        get {
+            return Double(display.text!)!
+        }
+    }
+    
+    // Object for the model of our calculator
+    private let brain = CalculatorBrain()
+    
+    
+    @IBAction private func operatorPressed(sender: UIButton) {
+        if let operatorValue = sender.currentTitle {
+            // If user typing a number then set accumulator
+            // in the model
+            if userInMiddleOfTypeANumber {
+                brain.setOperand(displayValue)
+                userInMiddleOfTypeANumber = false
+            }
+            
+            brain.performOperation(operatorValue)
+            displayValue = brain.result
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func decimalPointPressed(sender: UIButton) {
+        if userInMiddleOfTypeANumber {
+            if display.text!.containsString(".") == false {
+                self.digitPressed(sender)
+            }
+        }
+        else {
+            display.text = "0."
+            userInMiddleOfTypeANumber = true
+        }
     }
-
+    @IBAction private func digitPressed(sender: UIButton) {
+        if let value = sender.currentTitle {
+            if userInMiddleOfTypeANumber {
+                display.text = display.text! + value
+            }
+            else {
+                display.text = value
+            }
+            userInMiddleOfTypeANumber = true
+        }
+    }
 
 }
 
